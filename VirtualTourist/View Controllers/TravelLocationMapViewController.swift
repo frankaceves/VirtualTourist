b82efe006c3ee82eec8c03539784fa0d79c6c284
Frank Aceves <frankaceves@gmail.com>
2018-06-19 20:13:56 -0700

    add mapView to TravelLocationMap VC.

--- VirtualTourist/View Controllers/TravelLocationMapViewController.swift
+++ VirtualTourist/View Controllers/TravelLocationMapViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import MapKit
 
 class TravelLocationMapViewController: UIViewController {
-
+    @IBOutlet var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
