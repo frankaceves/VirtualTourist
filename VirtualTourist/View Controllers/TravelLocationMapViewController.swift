ecaed7f499937e16159da9f0cc868ce7cc3950e1
Frank Aceves <frankaceves@gmail.com>
2018-06-19 20:10:22 -0700

    Initial Commit

---
+++ VirtualTourist/View Controllers/TravelLocationMapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by Frank Anthony Aceves on 6/10/18.
+//  Copyright © 2018 Frank Aceves. All rights reserved.
+//
+
+import UIKit
+
+class TravelLocationMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
