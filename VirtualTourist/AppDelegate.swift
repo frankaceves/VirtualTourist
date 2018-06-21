2ce6752a9d4a70eb8f00eb726e55031e15bb6733
Frank Aceves <frankaceves@gmail.com>
2018-06-20 19:05:33 -0700

    add dataController variable to appDelegate.  Inject dataController dependency from appDelegate into TravelLocationMap VC.

--- VirtualTourist/AppDelegate.swift
+++ VirtualTourist/AppDelegate.swift
@@ -13,10 +13,19 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    let dataController = DataController(modelName: "VirtualTourist")
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        dataController.load()
+        
+        // Inject dataController dependency into TravelLocationMapViewController
+        let navigationController = window?.rootViewController as! UINavigationController
+        let travelViewController = navigationController.topViewController as! TravelLocationMapViewController
+        travelViewController.dataController = dataController
+        
         return true
     }
 
