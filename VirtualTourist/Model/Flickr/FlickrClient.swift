c8f1d0710ed08bd759fc156a38dc45fa16525f6b
Frank Aceves <frankaceves@gmail.com>
2018-06-24 16:57:05 -0700

    add FlickrClient file + URLFromParameters function

--- VirtualTourist/Model/Flickr/FlickrClient.swift
+++ VirtualTourist/Model/Flickr/FlickrClient.swift
@@ -9,5 +9,23 @@
 import UIKit
 
 class FlickrClient: NSObject {
-
+    
+    
+    // MARK: HELPER FUNCTIONS
+    
+    // create a URL from parameters
+    // SOURCE: used in The Movie Manager udacity sub-project (Section 5: Network Requests)
+    class func flickrURLFromParameters(_ parameters: [String:AnyObject], withPathExtension: String? = nil) -> URL {
+        var components = URLComponents()
+        components.scheme = FlickrClient.Constants.Flickr.APIScheme
+        components.host = FlickrClient.Constants.Flickr.APIHost
+        components.path = FlickrClient.Constants.Flickr.APIPath + (withPathExtension ?? "")
+        
+        for (key, value) in parameters {
+            let queryItem = URLQueryItem(name: key, value: "\(value)")
+            components.queryItems!.append(queryItem)
+        }
+        
+        return components.url!
+    }
 }
