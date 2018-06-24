b7c3f8b044c55fbfeb4d070ea554c64d3d10a229
Frank Aceves <frankaceves@gmail.com>
2018-06-24 16:47:21 -0700

    add Flickr client class file

---
+++ VirtualTourist/Model/Flickr/FlickrClient.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrClient.swift
+//  Virtual Tourist
+//
+//  Created by Frank Anthony Aceves on 6/24/18.
+//  Copyright © 2018 Frank Aceves. All rights reserved.
+//
+
+import UIKit
+
+class FlickrClient: NSObject {
+
+}
