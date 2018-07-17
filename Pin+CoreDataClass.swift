904cf15d5eee0d2d8d52247d3c725158f8743f61
Frank Aceves <frankaceves@gmail.com>
2018-07-16 19:50:48 -0700

    core data changes; made Pin - name optional

---
+++ Pin+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Pin+CoreDataClass.swift
+//  Virtual Tourist
+//
+//  Created by Frank Aceves on 6/25/18.
+//  Copyright © 2018 Frank Aceves. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Pin)
+public class Pin: NSManagedObject {
+
+}
